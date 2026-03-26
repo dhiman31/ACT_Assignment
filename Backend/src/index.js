f5993c8c34cad7ec4144cb8f78abaae7295e8bc1
dhiman31 <ankushiiituna@gmail.com>
2026-03-26 14:59:20 +0530

    basic setup done

---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const PORT = 3000
+
+const setUpAndStartServer = () => {
+    
+    const app = express()
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
+    app.listen(PORT ,() => {
+        console.log(`Server started at PORT ${PORT}`)
+    })
+
+}
+
+setUpAndStartServer()
